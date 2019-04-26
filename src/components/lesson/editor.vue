c46fc4ef1161c2233d64884e417244a41384d4df
ArtyomPLAY <artglz63@gmail.com>
2019-04-26 21:00:17 +0300

    guide on interface

--- src/components/lesson/editor.vue
+++ src/components/lesson/editor.vue
@@ -1,17 +1,18 @@
 <template>
-  <v-container fluid class="ma-0 pa-2">
+  <v-container class="fluid ma-0 pa-2">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/tonsky/FiraCode@1.206/distr/fira_code.css">
     <v-layout wrap column>
       <v-layout row>
-        <v-flex height="600" xs6>
+        <v-flex  xs6 id="code">
           <cm :codee="code" @update="updateCode"></cm>
         </v-flex>
-        <v-flex xs6 class="ml-3" style="overflow: auto !important; height: 300px">
+        <v-flex id="output" xs6 class="ml-3">
           <div v-html="code"></div>
         </v-flex>
       </v-layout>
       <v-layout row>
-        <v-flex xs6 class="mt-3">
-          <v-card class="lesson_text">
+        <v-flex xs6 class="mt-3" id="theory">
+          <v-card class="lesson_text" >
             <v-card-title primary-title class="pt-2">
               <div>
                 <div class="my-0" style="display: flex; flex-direction: row; align-items: center">
@@ -22,14 +23,14 @@
                   </div>
                 
 
-                <div class="lesson_text__content"
+                <div class="lesson_text__content" style="height: 400px;"
                   v-html="$options.filters.coder(lesson.text)"
                 ></div>
               </div>
             </v-card-title>
           </v-card>
         </v-flex>
-        <v-flex xs6 class="ml-3 mt-3">
+        <v-flex xs6 class="ml-3 mt-3" id="tasks">
           <v-card 
           class="task mb-2"
             :color="task.status==1 ? 'green':'primary'"
@@ -53,24 +54,53 @@
         </v-flex>
       </v-layout>
     </v-layout>
+     <v-tooltip top>
+        <template v-slot:activator="{ on }">
+          <v-btn flat
+          style="position: fixed; right: 10px; bottom: 10px;"
+          fab @click="guide_('start')" v-on="on" v-show="$route.name.indexOf('lesson') != -1">
+            <v-icon>help</v-icon>
+          </v-btn>
+        </template>
+        <span>Гайд по интерфейсу</span>
+      </v-tooltip>
+      <div v-show="guide.guide_active" class="guide_wrapper"></div>
+      <guidepopup
+      :iterator="guide.guide_i"
+      @next="guide_('next')" 
+      @back="guide_('back')"
+      @close="guide_('close')"
+      :style="guide.popup_style" 
+      id="guide_popup" 
+      v-if="guide.guide_active">
+      </guidepopup>
   </v-container>
 </template>
 
 <script>
 import cm from "./cm.vue";
 import solution from "./solution.vue"
+import { setTimeout } from 'timers'
+import guidepopup from './guide_popup.vue'
 
 export default {
   components: {
     cm,
-    solution
+    solution,
+    guidepopup
   },
   data() {
     return {
       code: '<h1 class="serif">Вы можете писать весь код прямо здесь!</h1>',
       i: 0,
       lesson: null,
-      dialog: false
+      dialog: false,
+      guide: {
+        guide_active: false,
+        guide_i: 0,
+        targets: ['code','output','theory','tasks'],
+        popup_style: {}
+      },
     };
   },
   methods: {
@@ -90,6 +120,52 @@ export default {
     },
     updateCode(value) {
       this.code = value;
+    },//доделать
+    guide_(func){
+      let v = this;
+      let target;
+      switch (func) {
+        case 'start':
+          v.guide.guide_active = true;
+          target = document.querySelector('#'+v.guide.targets[v.guide.guide_i]);
+          target.classList.add('guide');
+          this.popup_style();
+          break;
+        case 'next':
+          target = document.querySelector('#'+v.guide.targets[v.guide.guide_i]);
+          target.classList.remove('guide');
+          v.guide.guide_i++;
+          target = document.querySelector('#'+v.guide.targets[v.guide.guide_i]);
+          target.classList.add('guide');
+          this.popup_style();
+          break;
+        case 'back':
+          target = document.querySelector('#'+v.guide.targets[v.guide.guide_i]);
+          target.classList.remove('guide');
+          v.guide.guide_i--;
+          target = document.querySelector('#'+v.guide.targets[v.guide.guide_i]);
+          target.classList.add('guide');
+          this.popup_style();
+          break;
+        case 'close':
+          v.guide.guide_active = false;
+          target = document.querySelector('#'+v.guide.targets[v.guide.guide_i]);
+          target.classList.remove('guide');
+          v.guide.guide_i=0;
+        default:
+          break;
+      }
+      
+    },
+    popup_style(){
+      let target = document.querySelector('.guide');
+      let target_pos = target.getBoundingClientRect();
+      let y = (target_pos.y + (target_pos.height)/2)-145;
+      let x = (target_pos.x + (target_pos.width)/2)-125;
+      this.guide.popup_style = {
+        top: y+'px',
+        left: x+'px'
+      }
     }
   },
   watch: {
@@ -124,9 +200,35 @@ export default {
 </script>
 
 <style lang="scss">
+.guide {
+  z-index: 1000;
+  border: 2px solid #ffff00;
+  border-radius: 2px;
+  box-shadow: 0 0 20px #ffff004f;
+}
+
+#guide_popup {
+  z-index: 1001;
+  position: absolute !important;
+}
+
+.guide_wrapper {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 100;
+  background-color: #000000bb;
+}
+
 #editor_window {
   width: 100% !important;
   height: 100% !important;
+}
+
+#output {
+  overflow: auto;
 }
 
 .task {
